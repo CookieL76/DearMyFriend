481e41222f6782a2f894ac897d78921a3f634f9e
CookieL76 <gruenauer23146@hs-ansbach.de>
2026-08-02 00:13:12 +0200

    x

--- Interactive_Illustration_Toolbox/Assets/Scripts/PlayerController.cs
+++ Interactive_Illustration_Toolbox/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private float moveSpeed = 3.5f;
     private bool isFacingRight = true;
+
+    private const float minX = -8f;
+    private const float maxX = 8f;
 
     private SpriteRenderer spriteRenderer;
     private Animator animator;
@@ -20,7 +22,6 @@ public class PlayerController : MonoBehaviour
         movePlayer();
 
         float moveX = Input.GetAxis("Horizontal");
-
         if (moveX > 0 && !isFacingRight)
         {
             Flip();
@@ -30,24 +31,26 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
     }
+
     void movePlayer()
     {
         float moveX = Input.GetAxis("Horizontal");
-
         animator.SetBool("isMoving", moveX != 0);
 
         Vector3 move = new Vector3(moveX, 0f);
-
         transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+
+        // Clamp X position so the character can't move off screen
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        transform.position = pos;
     }
 
     void Flip()
     {
         isFacingRight = !isFacingRight;
-
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
     }
-
 }
