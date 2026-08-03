b940f781bf969eee9dbef633fecb9562c15ac30e
CookieL76 <gruenauer23146@hs-ansbach.de>
2026-08-03 12:46:38 +0200

    x

--- Interactive_Illustration_Toolbox/Assets/Scripts/ArrowTrigger.cs
+++ Interactive_Illustration_Toolbox/Assets/Scripts/ArrowTrigger.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class ArrowTrigger : MonoBehaviour
 {
     [SerializeField] private Collider2D arrowCollider;
+    [SerializeField] private SpriteRenderer arrowSprite;
 
     private void Start()
     {
         arrowCollider.enabled = false;
+
+        Color color = arrowSprite.color;
+        color.a = 0.3f;
+        arrowSprite.color = color;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -14,6 +19,10 @@ public class ArrowTrigger : MonoBehaviour
         if (other.GetComponent<PlayerController>() != null)
         {
             arrowCollider.enabled = true;
+
+            Color color = arrowSprite.color;
+            color.a = 1f;
+            arrowSprite.color = color;
         }
     }
 
@@ -22,6 +31,10 @@ public class ArrowTrigger : MonoBehaviour
         if (other.GetComponent<PlayerController>() != null)
         {
             arrowCollider.enabled = false;
+
+            Color color = arrowSprite.color;
+            color.a = 0.3f;
+            arrowSprite.color = color;
         }
     }
 }
